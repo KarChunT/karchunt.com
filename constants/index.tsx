bf1e73934ddd9a321fb5d148e755530186ef0f33
Tan Kar Chun <karchuntan.1999@gmail.com>
2023-11-16 21:51:20 +0800

    Setup navbar href, key, and label

---
+++ constants/index.tsx
@@ -0,0 +1,6 @@
+export const NAV_LINKS = [
+  { href: "/", key: "home", label: "Home" },
+  { href: "/about", key: "about", label: "About" },
+  { href: "/blog", key: "blog", label: "Blog" },
+  { href: "/gallery", key: "gallery", label: "Gallery" },
+];
