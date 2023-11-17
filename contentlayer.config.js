6e2db5a65486b6cd4030af775f74627e7a2aab57
Tan Kar Chun <karchuntan.1999@gmail.com>
2023-11-17 22:33:19 +0800

    Install ContentLayer dependency libraries and setup config for post schema

---
+++ contentlayer.config.js
@@ -0,0 +1,59 @@
+import { makeSource, defineDocumentType } from "@contentlayer/source-files";
+import readingTime from "reading-time";
+import remarkGfm from "remark-gfm"; // this is for table
+import rehypeSlug from "rehype-slug"; // adding id for each heading
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypePrettyCode from "rehype-pretty-code";
+
+const Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: "**/*.mdx",
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+    },
+    description: {
+      type: "string",
+    },
+    publishedAt: {
+      type: "date",
+      required: true,
+    },
+    isPublished: {
+      type: "boolean",
+      default: true,
+    },
+    tags: {
+      type: "list",
+      of: { type: "string" },
+    },
+  },
+  computedFields: {
+    url: {
+      type: "string",
+      resolve: (doc) => `/blog/${doc._raw.flattenedPath}`,
+    },
+    readingTime: {
+      type: "json",
+      resolve: (doc) => readingTime(doc.body.raw),
+    },
+  },
+}));
+
+const codeOptions = {
+  theme: "one-dark-pro",
+};
+
+export default makeSource({
+  contentDirPath: "posts",
+  documentTypes: [Post],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      [rehypeAutolinkHeadings, { behavior: "append" }],
+      [rehypePrettyCode, codeOptions],
+    ],
+  },
+});
