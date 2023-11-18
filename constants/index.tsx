a138222071f5cfae3da980a4ca5c4c015015ee68
Tan Kar Chun <karchuntan.1999@gmail.com>
2023-11-18 23:01:43 +0800

    Hide Gallery

--- constants/index.tsx
+++ constants/index.tsx
@@ -2,5 +2,5 @@ export const NAV_LINKS = [
   { href: "/", key: "home", label: "Home" },
   { href: "/about", key: "about", label: "About" },
   { href: "/blog", key: "blog", label: "Blog" },
-  { href: "/gallery", key: "gallery", label: "Gallery" },
+  // { href: "/gallery", key: "gallery", label: "Gallery" },
 ];
