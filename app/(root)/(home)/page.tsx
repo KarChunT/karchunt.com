688b642c0ac055aea48b863a4a9b981d29ed9728
Tan Kar Chun <karchuntan.1999@gmail.com>
2023-11-18 14:36:50 +0800

    Create new home and blog components and apply to homepage

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,37 @@
 "use client";
 
-import React from "react";
+import Hero from "@/components/shared/home/hero";
+import Newsletter from "@/components/shared/home/newsletter";
+import Post from "@/components/shared/blog/post";
+
+import { allPosts } from "@/.contentlayer/generated";
+import { descSortPosts } from "@/utils";
 
 const Home = () => {
-  return <div>test1</div>;
+  const posts = descSortPosts(allPosts).slice(0, 6);
+
+  return (
+    <div>
+      <Hero />
+      {/* <Gallery /> */}
+      <section className="padding-y">
+        <h2 className="text-[27px] font-semibold leading-tight md:text-4xl">
+          Latest Posts
+        </h2>
+        <div className="mt-6 max-w-7xl">
+          <div className="mt-6 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-10 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            {posts.map((post, index) => (
+              <article key={index}>
+                <Post post={post} />
+              </article>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <Newsletter />
+    </div>
+  );
 };
 
 export default Home;
