b524844d171985bce17c87cf8bb84e735b43dc4f
Tan Kar Chun <karchuntan.1999@gmail.com>
2023-11-19 18:07:31 +0800

    Setup SEO for blog post and tag page

--- app/tags/[slug]/page.tsx
+++ app/tags/[slug]/page.tsx
@@ -2,6 +2,32 @@ import PostCardLayout from "@/components/shared/blog/postCardLayout";
 import { allPosts } from "@/.contentlayer/generated";
 import { descSortPosts } from "@/utils";
 import { slug } from "github-slugger";
+import { siteMetaData } from "@/utils/siteMetaData";
+import post from "@/components/shared/blog/post";
+
+export async function generateStaticParams() {
+  const tags = [];
+  const paths = [{ slug: "all" }];
+
+  allPosts.map((post) => {
+    if (post.isPublished) {
+      post.tags?.map((tag) => {
+        const slugified = slug(tag);
+        if (!tags.includes(slugified)) {
+          tags.push(slugified);
+          paths.push({ slug: slugified });
+        }
+      });
+    }
+  });
+  return paths;
+}
+
+export async function generateMetadata({ params }) {
+  return {
+    title: `Tag: ${params.slug.replaceAll("-", " ")}`,
+  };
+}
 
 const page = ({ params }) => {
   let posts = allPosts.filter((post) => {
