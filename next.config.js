59b8817c73604e612f2967856653c906f22e71c2
Tan Kar Chun <karchuntan.1999@gmail.com>
2023-12-17 16:41:04 +0800

    Setup NextJS config for production

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
 
+let nextConfig = {}
+const isProduction = process.env.PRODUCTION || false;
+
+if (isProduction === "true") {
+  const repository = process.env.REPOSITORY;
+  nextConfig = {
+    output: "export",
+    distDir: "dist",
+    basePath: `/${repository}`,
+    assetPrefix: `/${repository}/`,
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    // images: {
+    //   unoptimized: true,
+    // }
+  }
+}
+
 const { withContentlayer } = require("next-contentlayer");
-
-const nextConfig = {};
-
 module.exports = withContentlayer({ ...nextConfig });
