988a1ba4b2f10ff09e859b8f2afd666f008a9c83
Tan Kar Chun <karchuntan.1999@gmail.com>
2023-12-23 14:01:37 +0800

    Set empty string to base path and asset prefix when isProduction = false

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@
 const isProduction = process.env.GITHUB_ACTIONS || "false";
 const isDomainSetup = process.env.NEXT_PUBLIC_HAS_DOMAIN || "false";
 
+if (isProduction === "false") {
+  process.env.NEXT_PUBLIC_BASE_PATH = "";
+  process.env.NEXT_PUBLIC_ASSET_PREFIX = "";
+}
+
 const nextConfig = {
   output: isProduction ? "export" : undefined,
   distDir: "dist",
