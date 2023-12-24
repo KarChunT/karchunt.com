13e27582b9cf6b06104f22e212e3cef1d2d81f0e
Tan Kar Chun <karchuntan.1999@gmail.com>
2023-12-24 12:55:52 +0800

    Support Next Image in MDX Content

--- components/shared/mdx/mdxcontent.tsx
+++ components/shared/mdx/mdxcontent.tsx
@@ -1,8 +1,15 @@
+"use client";
+
 import { useMDXComponent } from "next-contentlayer/hooks";
+import Image from "next/image";
+
+const mdxComponents = {
+  Image,
+};
 
 const MDXContent = ({ data }: { data: any }) => {
   const MDXDataContent = useMDXComponent(data.body.code);
-  return <MDXDataContent />;
+  return <MDXDataContent components={mdxComponents} />;
 };
 
 export default MDXContent;
