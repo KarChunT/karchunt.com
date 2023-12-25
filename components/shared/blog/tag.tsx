cc0d15d7e4cb091c0fed619b7d59d578a58450fb
Tan Kar Chun <karchuntan.1999@gmail.com>
2023-12-25 17:55:29 +0800

    Fix tag url

--- components/shared/blog/tag.tsx
+++ components/shared/blog/tag.tsx
@@ -4,9 +4,7 @@ import { Badge } from "@/components/ui/badge";
 const Tag = ({ name, tag }: { name: string; tag: string }) => {
   return (
     <Badge className="py-1.5">
-      <Link href={`/blog?tag=${tag}`}>
-        {name.replace("-", " ").toLowerCase()}
-      </Link>
+      <Link href={`/tags/${tag}`}>{name.replace("-", " ").toLowerCase()}</Link>
     </Badge>
   );
 };
