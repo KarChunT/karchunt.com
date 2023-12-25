afc31257466d9d36b12468bb5fd2b7211fc416ac
Tan Kar Chun <karchuntan.1999@gmail.com>
2023-12-25 17:55:16 +0800

    Fix search functionality

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
 import PostCardLayout from "@/components/shared/blog/postCardLayout";
+import { allPosts } from "@/.contentlayer/generated";
 
 export const metadata: Metadata = {
   title: "Blog",
 };
 
 const page = () => {
-  return <PostCardLayout header="Posts" />;
+  return <PostCardLayout header="Posts" posts={allPosts} tagFilter="all" />;
 };
 
 export default page;
--- components/shared/blog/postCardLayout.tsx
+++ components/shared/blog/postCardLayout.tsx
@@ -1,34 +1,110 @@
-import { Post } from "@/.contentlayer/generated";
+"use client";
+
 import BlogPost from "./blogPost";
-import SearchBlog from "./searchBlog";
-import TagFilter from "./tagFilter";
+import { descSortPosts, getAllUniqueTags } from "@/utils";
+import { useEffect, useState } from "react";
+import { Post, allPosts } from "@/.contentlayer/generated";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { slug } from "github-slugger";
 
 const PostCardLayout = ({
   header,
   posts,
+  tagFilter = "all",
 }: {
   header: string;
   posts: Post[];
+  tagFilter: string;
 }) => {
+  const allTags = getAllUniqueTags(allPosts);
+
+  const [filteredPosts, setFilteredPosts] = useState<Post[]>(
+    descSortPosts([posts])
+  );
+  const [postTitles] = useState<string[]>(
+    allPosts.map((post: Post) => post.title.toLowerCase())
+  );
+
+  const [searchText, setSearchText] = useState("");
+  const [tag, setTag] = useState<string>("all");
+
+  useEffect(() => {
+    const filteredPostsTitles: string[] = [...postTitles].filter(
+      (title: string) => title.indexOf(searchText.trim().toLowerCase()) !== -1
+    );
+
+    let refilteredPosts: Post[] = [];
+
+    if (header === "Tags") {
+      refilteredPosts = posts;
+    } else {
+      refilteredPosts = [...allPosts].filter((post: Post) =>
+        filteredPostsTitles.includes(post.title.toLowerCase())
+      );
+    }
+
+    refilteredPosts = refilteredPosts.filter((post: Post) => {
+      return post.tags.some((postTag: string) => {
+        const slugified = slug(postTag);
+        if (tag === "all") {
+          return true;
+        }
+        return slugified === tag;
+      });
+    });
+    setFilteredPosts(refilteredPosts);
+  }, [searchText, postTitles, posts, tag, header]);
+
   return (
     <div className="py-4 sm:py-10">
       <div className="flex flex-col gap-4">
         <h1 className="mt-4 text-[40px] font-bold leading-tight md:text-5xl">
-          {header}
+          {header !== "Tags" ? header : `${header}: ${tagFilter}`}
         </h1>
 
-        <div className="flex w-full flex-col gap-5 md:flex-row">
-          <SearchBlog />
-          <TagFilter />
-        </div>
+        {header !== "Tags" ? (
+          <div className="flex w-full flex-col gap-5 md:flex-row">
+            <div className="flex-1">
+              <Input
+                type="text"
+                placeholder="Post"
+                onChange={(e) => setSearchText(e.target.value)}
+              />
+            </div>
+            <div className="flex-1">
+              <Select onValueChange={(value: string) => setTag(value)}>
+                <SelectTrigger>
+                  <SelectValue placeholder="all" />
+                </SelectTrigger>
+                <SelectContent>
+                  {allTags.map((tag) => (
+                    <SelectItem value={tag} key={tag}>
+                      {tag.replace("-", " ").toLowerCase()}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
 
-      <div className="mt-16 max-w-7xl">
-        <div className="mt-6 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-          {posts.map((post, index) => (
-            <article key={index}>
+      <div className="mt-12">
+        <div className="grid grid-cols-1 gap-8">
+          {/* eslint-disable-next-line array-callback-return */}
+          {filteredPosts.map((post: Post, index: number) => (
+            <div key={index}>
               <BlogPost post={post} />
-            </article>
+            </div>
           ))}
         </div>
       </div>
