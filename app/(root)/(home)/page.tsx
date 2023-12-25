fb1a0df2e5adec6a41501b54b2b82c1d47c27809
Tan Kar Chun <karchuntan.1999@gmail.com>
2023-12-25 18:07:39 +0800

    Fix home page: latest posts section layout

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,27 +3,36 @@
 import Hero from "@/components/shared/home/hero";
 import Newsletter from "@/components/shared/home/newsletter";
 import BlogPost from "@/components/shared/blog/blogPost";
+import { Button } from "@/components/ui/button";
 
 import { allPosts } from "@/.contentlayer/generated";
 import { descSortPosts } from "@/utils";
+import Link from "next/link";
 
 const Home = () => {
-  const posts = descSortPosts(allPosts).slice(0, 6);
+  const posts = descSortPosts(allPosts).slice(0, 3);
 
   return (
     <div>
       <Hero />
       {/* <Gallery /> */}
       <section className="padding-y">
-        <h2 className="text-[27px] font-semibold leading-tight md:text-4xl">
-          Latest Posts
-        </h2>
-        <div className="mt-6 max-w-7xl">
-          <div className="mt-6 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-10 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-            {posts.map((post, index) => (
-              <article key={index}>
+        <div className="flex-between">
+          <h2 className="text-[27px] font-semibold leading-tight md:text-4xl">
+            Latest Posts
+          </h2>
+
+          <Button variant="default" asChild>
+            <Link href="/blog">View more</Link>
+          </Button>
+        </div>
+
+        <div className="mt-6">
+          <div className="grid grid-cols-1 gap-8">
+            {posts.map((post, index: number) => (
+              <div key={index}>
                 <BlogPost post={post} />
-              </article>
+              </div>
             ))}
           </div>
         </div>
