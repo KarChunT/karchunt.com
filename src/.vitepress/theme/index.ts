845ee70e5492c5bb4bbf86d40360f1133e0e956b
Tan Kar Chun <karchuntan.1999@gmail.com>
2024-02-24 15:56:49 +0800

    feat(theme): apply About component to custom global components

--- src/.vitepress/theme/index.ts
+++ src/.vitepress/theme/index.ts
@@ -1,11 +1,14 @@
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 
+import About from "./components/About.vue";
+
 import "./styles/globals.css";
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     // register all the custom global components
+    app.component("About", About);
   },
 } satisfies Theme;
