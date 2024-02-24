1018f10645a0fe13838c7d419428d89babfa2608
Tan Kar Chun <karchuntan.1999@gmail.com>
2024-02-24 15:09:08 +0800

    chore(conf): setup postcss and tailwindcss config

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/.vitepress/**/*.{js,ts,vue}", "./src/**/*.md"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
