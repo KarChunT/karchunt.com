74df002fb39f6ccfceffbd21d15b85f1ac67ca14
Tan Kar Chun <karchuntan.1999@gmail.com>
2024-03-03 16:44:00 +0800

    feat(blog-post): setup blog post layout
    create a new post layout for top, aside top, and aside bottom

---
+++ src/.vitepress/theme/components/BlogPostLayoutAsideBottom.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { getPost } from "../utils/posts";
+
+defineProps<{
+  insideDoc?: boolean;
+}>();
+
+const { nextPost, prevPost } = getPost();
+</script>
+
+<template>
+  <footer
+    class="mb-52 text-sm font-bold"
+    :class="{ 'xs:show lg:hidden': insideDoc }"
+  >
+    <div v-if="nextPost" class="py-3">
+      <h2 class="uppercase tracking-wide">Next Post</h2>
+      <a :href="`${nextPost.url}`">{{ nextPost.title }}</a>
+    </div>
+
+    <div v-if="prevPost" class="py-3">
+      <h2 class="uppercase tracking-wide">Previous Post</h2>
+      <a :href="`${prevPost.url}`">{{ prevPost.title }}</a>
+    </div>
+
+    <div class="pt-3 pb-7">
+      <a class="link" href="/blog">← Back to the blog</a>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+a {
+  color: var(--vp-c-brand-1);
+}
+</style>
---
+++ src/.vitepress/theme/components/BlogPostLayoutAsideTop.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { postsTags, getTagPath } from "../utils/tags";
+
+defineProps<{
+  insideDoc?: boolean;
+}>();
+</script>
+
+<template>
+  <div class="text-sm font-bold" :class="{ 'xs:show lg:hidden': insideDoc }">
+    <h2 class="uppercase tracking-wide">Tags</h2>
+    <div class="flex flex-wrap gap-2 pb-5">
+      <a class="" v-for="(_, tag) in postsTags()" :href="getTagPath(tag)">
+        {{ tag }}
+      </a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+a {
+  color: var(--vp-c-brand-1);
+}
+</style>
