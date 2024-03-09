8c752a39e3a15d34daf37db906a03d532e3d27d3
Tan Kar Chun <karchuntan.1999@gmail.com>
2024-03-09 16:02:35 +0800

    fix(blog): get post tags instead of all posts tags

--- src/.vitepress/theme/components/BlogPostLayoutAsideTop.vue
+++ src/.vitepress/theme/components/BlogPostLayoutAsideTop.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
-import { postsTags, getTagPath } from "../utils/tags";
+import { getTagPath } from "../utils/tags";
+import { getPost } from "../utils/posts";
 
 defineProps<{
   insideDoc?: boolean;
 }>();
+
+const { currentPost } = getPost();
 </script>
 
 <template>
   <div class="text-sm font-bold" :class="{ 'xs:show lg:hidden': insideDoc }">
     <h2 class="uppercase tracking-wide">Tags</h2>
     <div class="flex flex-wrap gap-2 pb-5">
-      <a class="" v-for="(_, tag) in postsTags()" :href="getTagPath(tag)">
+      <a class="" v-for="tag in currentPost.tags" :href="getTagPath(tag)">
         {{ tag }}
       </a>
     </div>
