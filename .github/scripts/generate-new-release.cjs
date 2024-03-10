5fccc3cd95d35cdf164ddb06a33466c23ec3990e
Tan Kar Chun <karchuntan.1999@gmail.com>
2024-03-10 22:03:30 +0800

    ci: create CI pipeline for deployment and to generate new release based on CHANGELOG.md

---
+++ .github/scripts/generate-new-release.cjs
@@ -0,0 +1,42 @@
+// @ts-nocheck
+/** @param {import('@types/github-script').AsyncFunctionArguments} AsyncFunctionArguments */
+module.exports = async ({ github, context, core }) => {
+  let body = "";
+  let insert = "false";
+  const version = process.env.RELEASE_TAG;
+  const version_with_v = `v${version}`;
+
+  try {
+    const changeLogContents = require("fs").readFileSync(
+      "CHANGELOG.md",
+      "utf-8"
+    );
+    changeLogContents.split("\n").forEach((line) => {
+      if (line.startsWith("## ")) {
+        if (line.includes(version)) {
+          insert = "true";
+        } else {
+          insert = "false";
+        }
+      }
+
+      if (insert === "true") {
+        body += line + "\n";
+      }
+    });
+
+    const response = await github.rest.repos.createRelease({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      tag_name: version_with_v,
+      name: version_with_v,
+      draft: false,
+      prerelease: false,
+      body: body,
+    });
+    core.exportVariable("RELEASE_ID", response.data.id);
+    core.exportVariable("RELEASE_UPLOAD_URL", response.data.upload_url);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
