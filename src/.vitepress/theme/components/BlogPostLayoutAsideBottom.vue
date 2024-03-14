78c1da8480832b173befc7f089c44185fa814f59
Tan Kar Chun <karchuntan.1999@gmail.com>
2024-03-14 14:39:43 +0800

    fix(single-blog): fix post links not reflect the page

--- src/.vitepress/theme/components/BlogPostLayoutAsideBottom.vue
+++ src/.vitepress/theme/components/BlogPostLayoutAsideBottom.vue
@@ -15,12 +15,12 @@ const { nextPost, prevPost } = getPost();
   >
     <div v-if="nextPost" class="py-3">
       <h2 class="uppercase tracking-wide">Next Post</h2>
-      <a :href="`${nextPost.url}`">{{ nextPost.title }}</a>
+      <a target="_self" :href="`${nextPost.url}`">{{ nextPost.title }}</a>
     </div>
 
     <div v-if="prevPost" class="py-3">
       <h2 class="uppercase tracking-wide">Previous Post</h2>
-      <a :href="`${prevPost.url}`">{{ prevPost.title }}</a>
+      <a target="_self" :href="`${prevPost.url}`">{{ prevPost.title }}</a>
     </div>
 
     <div class="pt-3 pb-7">
