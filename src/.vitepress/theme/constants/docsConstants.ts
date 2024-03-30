a76bbac3bbe395740c93340c407a89fe4b36e301
Tan Kar Chun <karchuntan.1999@gmail.com>
2024-03-30 16:46:33 +0800

    docs(ssh): setup SSH documentation

--- src/.vitepress/theme/constants/docsConstants.ts
+++ src/.vitepress/theme/constants/docsConstants.ts
@@ -1,13 +1,59 @@
 export const sshBasePath = "/docs/ssh";
-// export const sshSideBar = [
-//   {
-//     text: "Introduction",
-//     collapsed: false,
-//     items: [
-//       {
-//         text: "SSH Overview",
-//         link: `${sshBasePath}/ssh-overview`,
-//       },
-//     ],
-//   },
-// ];
+export const sshSideBar = [
+  {
+    text: "Introduction",
+    collapsed: false,
+    items: [
+      {
+        text: "SSH overview",
+        link: `${sshBasePath}/ssh-overview`,
+      },
+      {
+        text: "SSH installation",
+        link: `${sshBasePath}/installation`,
+      },
+    ],
+  },
+  {
+    text: "SSH Keys",
+    collapsed: false,
+    items: [
+      {
+        text: "Play with SSH Keys",
+        link: `${sshBasePath}/play-with-ssh-keys`,
+      },
+    ],
+  },
+  {
+    text: "Host & Server",
+    collapsed: false,
+    items: [
+      {
+        text: "Remote server connection",
+        link: `${sshBasePath}/remote-server-connection`,
+      },
+      {
+        text: "Host configuration",
+        link: `${sshBasePath}/host-configuration`,
+      },
+      {
+        text: "Server configuration",
+        link: `${sshBasePath}/server-configuration`,
+      },
+    ],
+  },
+  {
+    text: "SSH Tunnels",
+    collapsed: false,
+    items: [
+      {
+        text: "Local tunneling",
+        link: `${sshBasePath}/local-tunneling`,
+      },
+      {
+        text: "Remote tunneling",
+        link: `${sshBasePath}/remote-tunneling`,
+      },
+    ],
+  },
+];
---
+++ src/docs/ssh/remote-tunneling.md
@@ -0,0 +1,15 @@
+---
+title: Remote Tunneling
+titleTemplate: SSH
+description: Understand remote tunneling
+---
+
+<h1>Remote Tunneling</h1>
+
+## Remote tunneling to a server
+
+<style scoped>
+h2 {
+  margin-top: 36px;
+}
+</style>
