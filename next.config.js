83b2af1b2f43544d310e1f84c88eaf1b128fc04b
Tan Kar Chun <karchuntan.1999@gmail.com>
2024-06-23 16:47:42 +0800

    migration: migrate from VitePress to Nextra

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx',
+});
+
+// If you have other Next.js configurations, you can pass them as the parasmeter:
+module.exports = withNextra({
+  /* other next.js config */
+});
--- src/compiler.md
+++
@@ -1,6 +0,0 @@
----
-title: Compiler
-layout: home
----
-
-<Compiler />
