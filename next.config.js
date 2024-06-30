2bba2b9e4b1cddcc88f0b1ea8bc1c8c298771dff
Tan Kar Chun <karchuntan.1999@gmail.com>
2024-06-30 15:27:25 +0800

    chore: set defaultShowCopyCode to true for global config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  defaultShowCopyCode: true,
 });
 
 // If you have other Next.js configurations, you can pass them as the parasmeter:
