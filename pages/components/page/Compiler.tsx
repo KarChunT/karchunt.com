7116fc72983d9ab89f0d8377398a7d169ad05d66
Tan Kar Chun <karchuntan.1999@gmail.com>
2024-06-30 15:50:00 +0800

    migration: migrate compiler from vitepress to nextra

---
+++ pages/components/page/Compiler.tsx
@@ -0,0 +1,13 @@
+const Compiler = () => {
+  return (
+    <div className="mx-auto flex h-[80vh] w-full items-center justify-center p-6">
+      <iframe
+        height="550px"
+        src="https://onecompiler.com/embed/?theme=dark&hideTitle=true"
+        width="80%"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Compiler;
