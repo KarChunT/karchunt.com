97d5986a76d823fb4372dcdf9a779b8573002e6e
Tan Kar Chun <karchuntan.1999@gmail.com>
2024-07-14 14:42:44 +0800

    feat: support comment for each docs

---
+++ src/components/Comments.tsx
@@ -0,0 +1,42 @@
+'use client';
+import React, { useEffect, useRef } from 'react';
+
+const useScript = (params: any) => {
+  const { url, theme, issueTerm, repo, ref } = params;
+
+  useEffect(() => {
+    const commentScript = document.createElement('script');
+    commentScript.async = true;
+    commentScript.src = url;
+    commentScript.setAttribute('theme', theme);
+    commentScript.setAttribute('issue-term', issueTerm);
+    commentScript.setAttribute('crossorigin', 'anonymous');
+    commentScript.setAttribute('repo', repo);
+
+    console.log(ref.current.children.length);
+    if (ref.current.children.length === 0) {
+      ref.current.appendChild(commentScript);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+};
+
+const Comments = () => {
+  const comment = useRef(null);
+
+  useScript({
+    url: 'https://utteranc.es/client.js',
+    theme: 'github-dark',
+    issueTerm: 'pathname',
+    repo: 'KarChunT/posts-comments',
+    ref: comment,
+  });
+
+  return (
+    <div className="mt-16 w-full">
+      <div ref={comment} />
+    </div>
+  );
+};
+
+export default Comments;
