ccd9f567e31f20034e6395944035b0142e56efbb
Tan Kar Chun <karchuntan.1999@gmail.com>
2024-11-15 15:23:43 +0800

    refactor: refactor some UI layout and migrate _meta.json to _meta.tsx

---
+++ src/pages/docs/_meta.tsx
@@ -0,0 +1,26 @@
+export default {
+  ssh: {
+    type: 'page',
+    title: 'SSH',
+  },
+  git: {
+    type: 'page',
+    title: 'Git',
+  },
+  docker: {
+    type: 'page',
+    title: 'Docker',
+  },
+  'twelve-factor-app': {
+    type: 'page',
+    title: '12 Factor App',
+  },
+  kubernetes: {
+    type: 'page',
+    title: 'Kubernetes',
+  },
+  'data-structures-and-algorithms': {
+    type: 'page',
+    title: 'Data Structures & Algorithms',
+  },
+};
---
+++ src/pages/docs/ssh/_meta.tsx
@@ -0,0 +1,52 @@
+export default {
+  '-- Introduction': {
+    type: 'separator',
+    title: 'Introduction',
+  },
+  'ssh-overview': {
+    title: 'SSH overview',
+    href: '/docs/ssh/ssh-overview',
+  },
+  installation: {
+    title: 'SSH Installation',
+    href: '/docs/ssh/installation',
+  },
+  '-- SSH keys': {
+    type: 'separator',
+    title: 'SSH Keys',
+  },
+  'play-with-ssh-keys': {
+    title: 'Play with SSH Keys',
+    href: '/docs/ssh/play-with-ssh-keys',
+  },
+
+  '-- Host & Server': {
+    type: 'separator',
+    title: 'Host & Server',
+  },
+  'remote-server-connection': {
+    title: 'Remote server connection',
+    href: '/docs/ssh/remote-server-connection',
+  },
+  'host-configuration': {
+    title: 'Host configuration',
+    href: '/docs/ssh/host-configuration',
+  },
+  'server-configuration': {
+    title: 'Server configuration',
+    href: '/docs/ssh/server-configuration',
+  },
+
+  '-- SSH Tunnels': {
+    type: 'separator',
+    title: 'SSH Tunnels',
+  },
+  'local-tunneling': {
+    title: 'Local tunneling',
+    href: '/docs/ssh/local-tunneling',
+  },
+  'remote-tunneling': {
+    title: 'Remote tunneling',
+    href: '/docs/ssh/remote-tunneling',
+  },
+};
