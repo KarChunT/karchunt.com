cdd379f0f8872c6ced8d1863c444d292459dd1e7
Tan Kar Chun <karchuntan.1999@gmail.com>
2025-02-09 16:50:49 +0800

    feat: add Book interface to type definitions

--- src/types/type.d.ts
+++ src/types/type.d.ts
@@ -11,3 +11,14 @@ interface DocItem {
   link: string;
   hierarchy: HierarchyItem[];
 }
+
+interface Book {
+  title: string;
+  author: string;
+  url: string;
+  genre: string;
+  description: string;
+  coverColor: string;
+  coverUrl: string;
+  summary: string;
+}
