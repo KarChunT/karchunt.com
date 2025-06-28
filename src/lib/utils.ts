da3374bdf218818375afb834830a0cea81ead9f9
Tan Kar Chun <karchuntan.1999@gmail.com>
2025-06-28 15:17:22 +0800

    feat: add generateRandomArray function for random number generation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function generateRandomArray(size: number): number[] {
+  return Array.from(
+    { length: size },
+    () => Math.floor(Math.random() * 100) + 1,
+  );
+}
