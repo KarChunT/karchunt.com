1fb1e2f509ca7840cf6790fcf0f6202a9e8b8605
Tan Kar Chun <karchuntan.1999@gmail.com>
2025-06-28 15:17:14 +0800

    feat: add AlgorithmVisualizerSchema interface for algorithm visualization

--- src/types/type.d.ts
+++ src/types/type.d.ts
@@ -17,3 +17,14 @@ interface GalleryProps {
   src: string;
   category: string;
 }
+
+// JSON schema for algorithm visualizer
+interface AlgorithmVisualizerSchema {
+  [category: string]: {
+    [algorithm: string]: {
+      title: string;
+      description: string;
+      VisualizerComponent: React.ComponentType<any>;
+    };
+  };
+}
