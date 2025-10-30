40f61146137c62a799ddf6cac71e9366d88570eb
Kar Chun <karchuntan.1999@gmail.com>
2025-10-30 12:45:17 +0800

    refactor: remove hub items from meta configuration and clean up unused code

--- src/app/_meta.ts
+++ src/app/_meta.ts
@@ -1,5 +1,5 @@
 import type { MetaRecord } from 'nextra';
-import { DOCUMENTATION, HUB_ITEMS } from '@/constants';
+import { DOCUMENTATION } from '@/constants';
 
 const docsItems = Object.entries(DOCUMENTATION)
   .sort(([, a], [, b]) => a.title.localeCompare(b.title))
@@ -14,17 +14,6 @@ const docsItems = Object.entries(DOCUMENTATION)
     {} as Record<string, { title: string; href: string }>,
   );
 
-const hubItems = Object.entries(HUB_ITEMS).reduce(
-  (acc, [key, item]) => {
-    acc[key] = {
-      title: item.title,
-      href: item.href,
-    };
-    return acc;
-  },
-  {} as Record<string, { title: string; href: string }>,
-);
-
 /**
  * type MetaRecordValue =
  *  | TitleSchema
@@ -35,36 +24,19 @@ const hubItems = Object.entries(HUB_ITEMS).reduce(
  * type MetaRecord = Record<string, MetaRecordValue>
  **/
 const meta: MetaRecord = {
-  index: {
-    type: 'page',
+  '*': {
     display: 'hidden',
-  },
-  about: {
-    title: 'About',
-    type: 'page',
   },
   docs: {
     title: 'Docs',
     type: 'menu',
     items: docsItems,
-  },
-  blogger: {
-    title: 'Blog',
-    type: 'page',
-    href: '/blog',
-  },
-  blog: {
-    title: 'Blog1',
-    type: 'page',
     display: 'normal',
   },
-  hub: {
-    title: 'Hub',
-    type: 'menu',
-    items: hubItems,
-  },
-  tags: {
-    display: 'hidden',
+  blog: {
+    title: 'Blog',
+    type: 'page',
+    display: 'normal',
   },
 };
 
