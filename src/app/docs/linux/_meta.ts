cd5fc30d5ce18eae37bbff9781d9fd7650a59993
Kar Chun <karchuntan.1999@gmail.com>
2025-12-22 12:45:05 +0800

    docs: linux docs

---
+++ src/app/docs/linux/_meta.ts
@@ -0,0 +1,34 @@
+import type { MetaRecord } from 'nextra';
+
+/**
+ * type MetaRecordValue =
+ *  | TitleSchema
+ *  | PageItemSchema
+ *  | SeparatorSchema
+ *  | MenuSchema
+ *
+ * type MetaRecord = Record<string, MetaRecordValue>
+ **/
+const meta: MetaRecord = {
+  '*': {
+    theme: {
+      sidebar: true,
+      layout: 'default',
+    },
+  },
+  '#': {
+    type: 'separator', // or 'doc' if it's a document
+    title: 'Introduction',
+  },
+  'what-is-linux': {
+    title: 'What is Linux?',
+  },
+  'linux-shell': {
+    title: 'Linux Shell',
+  },
+  'basic-commands': {
+    title: 'Basic Commands',
+  },
+};
+
+export default meta;
